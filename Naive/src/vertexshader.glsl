7e819585714655626b3cde329a6ce552b4d46004
Damian Stachura <damian260@onet.pl>
2018-07-24 00:50:56 +0200

    Different sizes of bodies in simulation

--- Naive/src/vertexshader.glsl
+++ Naive/src/vertexshader.glsl
@@ -8,6 +8,15 @@ uniform mat4 MVP;
 
 void main() {
   gl_Position = MVP * vec4(position, 1);
+  if(mass < 1000.0) {
+    gl_PointSize = 2.0;
+  } else if((mass >= 1000.0f) && (mass <= 10000.0)) {
+    gl_PointSize = 3.0;
+  } else if((mass >= 10000.0) && (mass <= 50000.0)) {
+    gl_PointSize = 5.0;
+  } else {
+    gl_PointSize = 7.0;
+  }
   vMass = mass;
   vertex_color = color;
 }
