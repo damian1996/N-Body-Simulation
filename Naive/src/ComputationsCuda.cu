ad742b29ba6def114264bfbad0be1ef418619bf2
Damian Stachura <damian260@onet.pl>
2018-08-15 14:07:34 +0200

    Paper first part (#35)

--- Naive/src/ComputationsCuda.cu
+++ Naive/src/ComputationsCuda.cu
@@ -61,15 +61,3 @@ void Computations::NaiveSimBridgeThrust(type &pos, int numberOfBodies, float dt)
   //if(!testingMomemntum(numberOfBodies)) std::cout << "problemiki" << "\n";
   pos = posD;
 }
-
-// https://www.bu.edu/pasi/files/2011/07/Lecture6.pdf
-// https://docs.nvidia.com/cuda/cuda-c-best-practices-guide/index.html
-// https://stackoverflow.com/questions/4176762/passing-structs-to-cuda-kernels
-// https://codeyarns.com/2011/02/16/cuda-dim3/
-// http://developer.download.nvidia.com/CUDA/training/introductiontothrust.pdf
-// https://groups.google.com/forum/#!topic/thrust-users/4EaWLGeJOO8
-// https://github.com/thrust/thrust/blob/master/examples/cuda/unwrap_pointer.cu
-// thrust::copy(weightsD.begin(), weightsD.end(),
-// std::ostream_iterator<float>(std::cout, " "));
-//testingMomemntum(numberOfBodies);
-//if(!testingMomemntum(numberOfBodies)) std::cout << "problemiki" << "\n";
