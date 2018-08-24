06be3b20fcfe5b56967e1079af0d0dd2bd67409b
Damian Stachura <damian260@onet.pl>
2018-08-24 16:20:00 +0200

    I compiled code and repaired several bugs. Corrections in the paper propsed by promotor

--- SimulationCode/test.sh
+++ SimulationCode/test.sh
@@ -1,21 +1,33 @@
 #make clean
+make -j5
 if [ $1 == "debug" ]
 then
-    make
     gdb out/main
+elif [ $1 == "debugCuda" ]
+then
+    export CUDA_DEBUGGER_SOFTWARE_PREEMPTION=1
+    /usr/local/cuda/bin/cuda-gdb out/main
+elif [ $1 == "FinTest" ]
+then
+    echo "4" > input
+    echo "10" >> input 
+    ./out/main < input
+    for i in {1..10}
+    do
+        echo "4" > input
+        echo $(($i*10000)) >> input
+        ./out/main < input
+    done
 elif [ $1 == "test" ]
 then
-    make
     for i in {1..10}
     do
         ./out/main < input
     done
 elif [ $1 == "prof" ]
 then
-    make
     /usr/local/cuda-9.2/bin/nvprof ./out/main < input
 else
-    make
     ./out/main < input
 fi
 
